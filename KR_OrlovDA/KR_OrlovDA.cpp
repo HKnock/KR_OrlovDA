8b1700bdfc1f6b126eb6516085700350cca5dd1a
HKnock <34215956+HKnock@users.noreply.github.com>
2020-11-04 23:24:54 +0300

    Добавьте файлы проекта.

---
+++ KR_OrlovDA/KR_OrlovDA.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <process.h>
+#include <stdarg.h>
+#include <string.h>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "ru");
+    FILE* f = fopen(argv[1], "r");
+    char* pEnd;
+    char c, ch[50];
+    int counter = -1, max = strtoll(argv[2], &pEnd, 10);
+    cout << "Original file:\n";
+    while (true)
+    {
+        counter++;
+        ch[counter] = fgetc(f);
+        if (ch[counter] == EOF)
+            break;
+        cout << ch[counter] << " ";
+    }
+    fclose(f);
+    f = fopen(argv[1], "w");
+    cout << "\nNew file:\n";
+    for (int i = 0; i < counter; i++) {
+        if (i == max) {
+            fprintf(f, "Z");
+            cout << "Z ";
+        }
+        fprintf(f, "%c", ch[i]);
+        cout << ch[i] << " ";
+    }
+    fclose(f);
+}
